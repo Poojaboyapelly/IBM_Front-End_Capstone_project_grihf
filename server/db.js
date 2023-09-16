302288761a1b72a88b79567f96bf4cf951a4e2e1
andre1melo1 <[andre1melo1@proton.me](mailto:andre1melo1@proton.me)>
2023-09-16 09:54:11 -0400

    Update 7 - Exercise 2,3,4 complete - Convert Static Pages to Dynamic React Components

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MTIxNzgtYW5kcmUx@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MzI3MTAtYW5kcmUx@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Landing_Page from './Components/Landing_Page/Landing_Page';
 
+import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
@@ -27,6 +29,8 @@ function App() {
           <Navbar/>
               <Routes>
               <Route path="/" element={<Landing_Page/>}/>
+              <Route path="/Login" element={<Login/>}/>
+              <Route path="/Sign_Up" element={<Sign_Up/>}/>
               </Routes>
             
         </BrowserRouter>
